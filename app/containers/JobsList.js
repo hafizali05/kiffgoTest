61166ea75cc04168493f1adcee4610dab4bfec66
hafiz didarali <hafizdidarali@gmail.com>
2018-06-09 20:37:14 +0100

    created Joblist component

---
+++ app/containers/JobsList.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+
+class JobsList extends Component{
+    constructor(){
+        super()
+    }
+    render(){
+        return(
+            <View>
+                <Text>Hello Jobs List</Text>
+            </View>
+        )
+    }   
+}
+
+export default JobsList;
