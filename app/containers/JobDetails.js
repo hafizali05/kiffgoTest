3a09d21c71d953590771f720fcfaa90c41e4c15f
hafiz didarali <hafizdidarali@gmail.com>
2018-06-10 10:26:55 +0100

    added de-structured imports to job details

--- app/containers/JobDetails.js
+++ app/containers/JobDetails.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Text, View } from "react-native";
-import Card from "./common/Card";
-import CardSection from "./common/CardSection";
+import { Card } from "./common/Card";
+import { CardSection } from "./common/CardSection";
 
 const JobDetails = props => {
   const { headerContentStyle } = styles;
