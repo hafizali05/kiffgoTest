0306088b9f0991197f5bdce2a96c77cfce5dc33c
hafiz didarali <hafizdidarali@gmail.com>
2018-06-10 06:46:22 +0100

    basic fetching of data is working

--- app/containers/JobsList.js
+++ app/containers/JobsList.js
@@ -1,18 +1,28 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
+import React, { Component } from "react";
+import { Text, View } from "react-native";
+import Axios from "axios";
 
-
-class JobsList extends Component{
-    constructor(){
-        super()
-    }
-    render(){
-        return(
-            <View>
-                <Text>Hello Jobs List</Text>
-            </View>
-        )
-    }   
+class JobsList extends Component {
+  state = {
+    jobs: []
+  };
+  componentWillMount() {
+    console.log("componentWillMount");
+    Axios.get("https://rallycoding.herokuapp.com/api/music_albums").then(
+      response => console.log(this.setState({ jobs: response.data }))
+    );
+  }
+  renderJobs() {
+    return this.state.jobs.map(job => <Text>{job.title}</Text>);
+  }
+  render() {
+    return (
+      <View>
+        {/* <Text>Hello Jobs List helo</Text> */}
+        {this.renderJobs()}
+      </View>
+    );
+  }
 }
 
 export default JobsList;
